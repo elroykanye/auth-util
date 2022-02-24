c38751f992876b2f2f2c6fcb9d66d02df4bcfa79
Elroy Kanye <elroykanye@gmail.com>
2022-02-24 23:16:51 +0100

    removed serial from serializable

--- src/main/java/com/elroykanye/jwt/JwtTokenUtil.java
+++ src/main/java/com/elroykanye/jwt/JwtTokenUtil.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.function.Function;
